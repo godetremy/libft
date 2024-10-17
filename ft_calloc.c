32c5b4c6a6a2485c24c32326434e4dbeda7576c7
Rémy Godet <godetremy@free.fr>
2024-10-17 10:50:26 +0200

    fix(split): Unused import

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,18 @@
 /*   By: remy <rgodet@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:34:33 by remy              #+#    #+#             */
-/*   Updated: 2024/10/10 16:54:17 by remy             ###   ########.fr       */
+/*   Updated: 2024/10/17 10:27:34 by remy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_calloc(t_size malloc_size, t_size type_size)
 {
 	void	*res;
 
 	res = malloc(malloc_size * type_size);
-	if (!res)
+	if (res == 0)
 		return (0);
 	ft_bzero(res, malloc_size * type_size);
 	return (res);
