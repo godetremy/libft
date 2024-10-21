1c6ca62914096caf345b221f18e23bfd12da4953
rgodet <marvin@student.42.fr>
2024-10-21 11:01:10 +0200

    fix(strmapi): Add NULL verification

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: remy <rgodet@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:35:43 by remy              #+#    #+#             */
-/*   Updated: 2024/10/10 11:12:32 by remy             ###   ########.fr       */
+/*   Updated: 2024/10/21 10:56:35 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*res;
 	int		i;
 
+	if (!s || !f)
+		return (NULL);
 	res = ft_strdup(s);
 	if (!res)
 		return (0);
