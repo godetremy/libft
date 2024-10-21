75fc6ff32b7cb7c8a29d524ff07444e65030fbf7
rgodet <marvin@student.42.fr>
2024-10-21 11:01:47 +0200

    fix(strjoin): Add NULL verification

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: remy <rgodet@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:34:57 by remy              #+#    #+#             */
-/*   Updated: 2024/10/11 09:28:46 by remy             ###   ########.fr       */
+/*   Updated: 2024/10/21 10:55:36 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		x;
 	char	*res;
 
+	if (!s1 || !s2)
+		return (NULL);
 	res = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!res)
 		return (0);
