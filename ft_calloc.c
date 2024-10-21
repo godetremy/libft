37c12ba068d69c589e74bec305853ad41ab1ac03
rgodet <marvin@student.42.fr>
2024-10-21 11:04:23 +0200

    fix(calloc): Fix when NULL size

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: remy <rgodet@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:34:33 by remy              #+#    #+#             */
-/*   Updated: 2024/10/17 10:27:34 by remy             ###   ########.fr       */
+/*   Updated: 2024/10/21 10:31:54 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ void	*ft_calloc(t_size malloc_size, t_size type_size)
 {
 	void	*res;
 
+	if (malloc_size == 0 || type_size == 0)
+		return (malloc(0));
+	if (malloc_size > (size_t) - 1 / type_size)
+		return (NULL);
 	res = malloc(malloc_size * type_size);
-	if (res == 0)
+	if (!res)
 		return (0);
 	ft_bzero(res, malloc_size * type_size);
 	return (res);
