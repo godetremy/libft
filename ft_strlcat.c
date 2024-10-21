d1e44c5658c8c050fc2ce27cd04f2f82beddc850
rgodet <marvin@student.42.fr>
2024-10-21 11:01:23 +0200

    fix(strlcat): Add NULL verification

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,20 +6,20 @@
 /*   By: remy <rgodet@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:32:57 by remy              #+#    #+#             */
-/*   Updated: 2024/10/17 12:09:57 by rgodet           ###   ########.fr       */
+/*   Updated: 2024/10/21 10:13:57 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlcat(char *dst, const char *src, t_size size)
+t_size	ft_strlcat(char *dst, const char *src, t_size size)
 {
 	t_size	i;
 	t_size	x;
 	t_size	original_size;
 
-	if ((dst == NULL && size == 0) || (src == NULL && size == 0))
-		return (0);
+	if (dst == NULL && size == 0)
+		return (ft_strlen(src));
 	original_size = ft_strlen(dst);
 	i = original_size;
 	x = 0;
